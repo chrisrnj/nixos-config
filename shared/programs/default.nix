f20355cebef2421091e4bf34d7678edd2cdf097c
Christiano Rangel <christianorangel322@gmail.com>
2026-08-10 13:19:43 -0300

    Add libreoffice, update flake

--- shared/programs/default.nix
+++ shared/programs/default.nix
@@ -17,6 +17,10 @@
   environment.systemPackages = with pkgs; [
     # Productivity
     gimp-with-plugins
+    libreoffice-qt
+    hunspell
+    hunspellDicts.pt_BR
+    hunspellDicts.en_US
     tenacity
 
     # Development
