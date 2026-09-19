5a384bcf154553f7d975088f3f1b28fcf2467a35
chrisrnj <christianorangel322@gmail.com>
2026-09-18 22:23:43 -0300

    Fix some amdgpu settings

--- desktop/amdgpu/default.nix
+++ desktop/amdgpu/default.nix
@@ -4,17 +4,21 @@
   environment.systemPackages = with pkgs; [
     btop-rocm
     libva-utils
+    vdpauinfo
   ];
 
   hardware = {
-    # ROCM OpenCL
-    amdgpu.opencl.enable = true;
+    amdgpu = {
+      initrd.enable = true;
+      opencl.enable = true;
+      zluda.enable = true;
+    };
     graphics = {
       enable = true;
       enable32Bit = true;
-#       extraPackages = with pkgs; [
-#         libvdpau-va-gl
-#       ];
+      extraPackages = with pkgs; [
+        libvdpau-va-gl
+      ];
     };
   };
 
@@ -22,12 +26,7 @@
 
   environment.variables = {
     MESA_SHADER_CACHE_MAX_SIZE = "16G";
-#    AMD_VULKAN_ICD = "RADV";
-#    VDPAU_DRIVER = "va_gl";
+#     AMD_VULKAN_ICD = "RADV";
+    VDPAU_DRIVER = "va_gl";
   };
-
-  # Fix plymouth not showing.
-  boot.initrd.kernelModules = [
-    "amdgpu"
-  ];
 }
