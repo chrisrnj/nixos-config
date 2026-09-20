be8a24b1862914d7dae9af987e2f9b72ad16f195
Christiano Rangel <christianorangel322@gmail.com>
2026-09-19 22:29:25 -0300

    Add apple-avd support

--- laptop/asahi/default.nix
+++ laptop/asahi/default.nix
@@ -1,10 +1,12 @@
 { ... }:
 
 {
-  hardware.asahi.enable = true;
-
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = false;
+  hardware.asahi = {
+    enable = true;
+    # Video Acceleration.
+    avd.vaapi-support = true;
   };
+
+  # Use systemd-boot.
+  boot.loader.systemd-boot.enable = true;
 }
