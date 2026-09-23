13d0019a13d66b3880202295abdf87355be7ddf1
chrisrnj <christianorangel322@gmail.com>
2026-09-22 21:03:48 -0300

    Move local firmware folder up

--- laptop/asahi/default.nix
+++ laptop/asahi/default.nix
@@ -6,9 +6,9 @@
     # Video Acceleration.
     avd.vaapi-support = true;
     peripheralFirmwareDirectory = lib.findFirst (path: builtins.pathExists (path + "/firmware.cpio")) null [
+      /etc/nixos/laptop/asahi/firmware
       /boot/vendorfw
       /mnt/boot/vendorfw
-      /etc/nixos/laptop/asahi/firmware
     ];
   };
 
